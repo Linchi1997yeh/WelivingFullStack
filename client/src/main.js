4acc5192159566da9e675bb4022e59215b4a6fc0
melo <melolinchou@gmail.com>
2019-11-03 11:50:30 +0800

    Fix ionic

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import VueResource from 'vue-resource'
+import Ionic from '@ionic/vue';
+import '@ionic/core/css/ionic.bundle.css';
+
+Vue.use(Ionic);
+Vue.config.productionTip=false;
+
+Vue.use(VueResource);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import { IonicVueRouter } from '@ionic/vue';
-import Home from '../views/Home.vue';
 import Login from '../views/Login.vue'
 
 Vue.use(IonicVueRouter);
@@ -10,11 +9,7 @@ export default new IonicVueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes : [
-    {
-      path: '/home',
-      name: 'home',
-      component: Home
-    },
+    
     {
       path: '/',
       name: 'login',
